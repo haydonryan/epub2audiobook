68a3a6d02505a06e52f3f4b9a9f3a310a411f69d
Haydon Ryan <haydon.ryan@gmail.com>
2024-11-15 13:01:15 -0600

    Initial hacky poc commit

---
+++ src/main.rs
@@ -0,0 +1,114 @@
+use epub::doc::EpubDoc;
+use scraper::{Html, Selector};
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::str;
+
+fn get_title(html: &str) -> String {
+    let document = Html::parse_document(html);
+    let selector = Selector::parse("title").unwrap();
+
+    let title: String = document
+        .select(&selector)
+        .flat_map(|element| element.text().collect::<Vec<_>>())
+        .collect::<String>();
+
+    title.clone()
+}
+
+fn output_to_file(filename: String, contents: &str) {
+    let mut file = match File::create(filename) {
+        Ok(file) => file,
+        Err(error) => panic!("Could not create file: {}", error),
+    };
+    if let Err(error) = file.write_all(contents.as_bytes()) {
+        panic!("Error writing contents to file: {}", error);
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut filename = "";
+    if args.len() > 1 {
+        filename = &args[1];
+    }
+
+    println!("EPUB to TXT Converter");
+    println!("---------------------");
+
+    // Load the EPUB
+    let doc = EpubDoc::new(filename);
+    assert!(doc.is_ok());
+    let mut doc = doc.unwrap();
+
+    // Grab title and author
+    let title = doc.mdata("title");
+    let author = doc.mdata("creator");
+
+    // Get Cover
+    /*
+        let cover_data = doc.get_cover().unwrap();
+
+        let f = fs::File::create("/tmp/cover.png");
+        assert!(f.is_ok());
+        let mut f = f.unwrap();
+        //let resp = f.write_all(&cover_data);
+    */
+    //dbg!(doc.resources);
+
+    let number_of_ids = doc.spine.len();
+    let spine = doc.spine.clone();
+
+    println!("Title: {}", title.unwrap());
+    println!("Author: {}", author.unwrap());
+    println!("Number of Sections: {}", number_of_ids);
+    println!();
+    //dbg!(spine.clone());
+    let mut i = 1;
+    for current_section in spine {
+        let path = doc.resources.get(&current_section).unwrap().0.clone();
+        let text = doc.get_resource_by_path(path).unwrap();
+        let html = str::from_utf8(&text).unwrap();
+        print!(
+            "Converting chapter {}/{}: {} ",
+            i,
+            number_of_ids,
+            current_section,
+            //    path.to_str().unwrap().clone(),
+            //doc.resources.get(&current_section).unwrap().1
+        );
+
+        let filename = format!("test/{:04}_{}.txt", i, current_section);
+        //println!("Filename: {}    Title: {}", filename, current_section);
+        println!("Filename: {}", filename);
+        let document = Html::parse_document(html);
+        let selector = Selector::parse("body").unwrap();
+
+        let _text: String = document
+            .select(&selector)
+            .flat_map(|element| element.text().collect::<Vec<_>>())
+            .collect::<String>();
+
+        output_to_file(filename, &_text);
+
+        //println!("--------------------------------------------------");
+        //print!("HTML: <{}>", html);
+        //println!("--------------------------------------------------");
+        //print!("TEXT: <{}>", _text);
+        //println!("--------------------------------------------------");
+
+        i += 1;
+    }
+    //dbg!(&doc);
+    return;
+    let toc = doc.toc;
+    //dbg!(&toc);
+    for t in toc {
+        println!(
+            "{}                         | {}",
+            t.label,
+            t.content.to_str().unwrap()
+        );
+    }
+}
