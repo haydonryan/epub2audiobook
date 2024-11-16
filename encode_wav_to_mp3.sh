7214e5285adb47c48225211959d2d3b5035c3d9e
Haydon Ryan <haydon.ryan@gmail.com>
2024-11-15 21:26:42 -0600

    adding script for wav to mp3 conversion

---
+++ encode_wav_to_mp3.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+echo "Run this from your output directory, not the base repository directory."
+
+mkdir MP3
+cd wav/ > /dev/null
+let i=1
+
+echo "Grabbing book title, author and cover file."
+source ../book.sh
+
+echo "Encoding MP3s from WAVs.."
+for f in *.wav; do
+  title_file=$(echo ${f/.txt.wav/} )
+  CHAPTER_TITLE=$(cat "../$title_file.title")
+#  echo $CHAPTER_TITLE
+
+  # Strip .txt.wav out of output file
+  title=${title//_/ }
+  outfile=$(echo ${f/.txt.wav/} )
+  echo "$outfile"
+
+  ffmpeg -loglevel info -ac 1 -channel_layout mono -y -i "$f" -i "../Cover.jpg" -c:v copy -c:a libmp3lame -id3v2_version 3  -metadata:s:v comment="Cover (front)"  -metadata track=$i -metadata title="$CHAPTER_TITLE" -metadata album_artist="$BOOK_AUTHOR" -metadata artist="$BOOK_AUTHOR" -metadata album="$BOOK_TITLE" -q:a 2 "../MP3/$outfile.mp3" &
+  let i=i+1
+done
+wait
+echo "Done."
