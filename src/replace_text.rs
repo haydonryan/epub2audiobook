6786d1e629d905c32e5fe7394aabd0f7a495a2cb
Haydon Ryan <haydon.ryan@gmail.com>
2024-12-04 10:22:41 -0600

    Adding support for decimals in million dollar amounts

--- src/replace_text.rs
+++ src/replace_text.rs
@@ -5,7 +5,7 @@ fn convert_money_to_words(text: &str) -> String {
     let search_text = re.replace_all(text, "one dollar").to_string();
     println!("1: {}", search_text);
 
-    let re = Regex::new(r"\$([1-9][0-9]*\s(million|billion|trillion))").unwrap();
+    let re = Regex::new(r"\$([1-9][\.]*[0-9]*\s(million|billion|trillion))").unwrap();
     let search_text = re.replace_all(&search_text, "$1 dollars").to_string();
     println!("2: {}", search_text);
 
@@ -18,6 +18,12 @@ fn test_convert_money_to_words() {
     // Special Case for a singular
     let text = "$1";
     assert_eq!("one dollar".to_string(), convert_money_to_words(text));
+
+    let text = "$1.25 million";
+    assert_eq!(
+        "1.25 million dollars".to_string(),
+        convert_money_to_words(text)
+    );
 
     let text = "$1 million";
     assert_eq!(
